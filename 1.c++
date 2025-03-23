92eac39d78f7e907121abf74ec2ef581a9bfe308
Chetan Gadhiya <140878255+chetangadhiya4939@users.noreply.github.com>
2025-03-23 11:34:05 +0530

    First Commit.
    This is what I practiced for C++ language.

---
+++ 1.c++
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout<< "Jay Swaminarayan"<< endl << "Chetan ";
+cout<< "Guruji";
+return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<string.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+  // int x;
+  // string txt = "It's alright.";
+  // cout << txt << endl;
+  // cout << "Type a number: "; // Type a number and press enter
+  // cin >> x; // Get user input from the keyboard
+  // cout << "Your number is: " << x;
+//   string greeting1 = "Hello";  // Regular String
+// char greeting2[] = "Hello";
+
+// // cout << greeting1 << endl;
+// cout << greeting2 << endl;
+
+// cout << sqrt(64)<< endl;
+// cout << round(2.4)<< endl;
+// cout << log(2)<< endl;
+// A variable with some specific numbers
+for (int i = 0; i < 10; i++) {
+  if (i == 4) {
+    continue;
+  }
+  cout << i << "\n";
+}
+}
